5203d61f4f5afadbaf7c22f4abbd4fca42223da1
AlexGudini <enavals1@mail.ru>
2018-04-04 00:24:48 +0300

    Add files via upload

---
+++ LabLOIS/operation.js
@@ -0,0 +1,28 @@
+function clickCheckFormula() {
+    var test = document.getElementById("inputFormula").value;
+    var outputCheckTypeFormula = document.getElementById("outputCheckTypeFormula");
+	var subFormula = document.getElementById("subFormula");
+	var correctFormula= document.getElementById("correctFormula");
+    if (verificationFormula(test)) {
+		correctFormula.innerHTML = "Данное выражение является формулой";
+		subFormula.innerHTML = "Количество подформул: "+ searchSubformuls(test);
+        document.getElementById("hiddenForm").hidden = false;		
+        var object = getTableTruth(test);
+		if (getCount() == getN()) {
+		   outputCheckTypeFormula.innerHTML = "Данная формула является противоречивой"; 
+        } else {
+            outputCheckTypeFormula.innerHTML = "Данная формула не является противоречивой";
+        }
+        var tbody = document.getElementById('tbody');
+        tbody.innerHTML = null;
+        tbody.innerHTML = objectToTable(object.table, object.sizeSymbolInFormula);
+
+    } else {
+		correctFormula.innerHTML = "Данное выражение не является формулой";
+        document.getElementById("hiddenForm").hidden = true;
+    }
+}
+
+
+
+
